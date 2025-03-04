c1914bc4836ec6672d87bcb665c540d242c60513
learnandsail <fcs090sutaromkar@gmail.com>
2025-03-04 13:00:03 +0530

    course changes

--- models/course.js
+++ models/course.js
@@ -1,3 +1,24 @@
+// const mongoose = require("mongoose");
+
+// const courseSchema = new mongoose.Schema({
+//     image: String,
+//     name: { type: String, required: true },
+//     duration: String,
+//     fees: String,
+//     discount: String,
+//     installments: String,
+//     recordingsAvailability: String,
+//     language: String,
+//     classModule: String,
+//     platform: String,
+//     studyMaterial: String,
+//     certificate: String,
+//     discussionPlatform: String,
+//     keyPoints: String,
+// }, { timestamps: true });
+
+// module.exports = mongoose.model("Course", courseSchema);
+
 const mongoose = require("mongoose");
 
 const courseSchema = new mongoose.Schema({
@@ -5,15 +26,15 @@ const courseSchema = new mongoose.Schema({
     name: { type: String, required: true },
     duration: String,
     fees: String,
-    discount: String,
-    installments: String,
-    recordingsAvailability: String,
-    language: String,
-    classModule: String,
-    platform: String,
-    studyMaterial: String,
-    certificate: String,
-    discussionPlatform: String,
+    // discount: String,
+    // installments: String,
+    // recordingsAvailability: String,
+    // language: String,
+    // classModule: String,
+    // platform: String,
+    // studyMaterial: String,
+    // certificate: String,
+    // discussionPlatform: String,
     keyPoints: String,
 }, { timestamps: true });
 
